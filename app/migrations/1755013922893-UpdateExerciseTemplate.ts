29e5a19eff8f2e1005f7833b5532ae7565ef6505
Artem P <pasechnik@mcn.ru>
2025-08-12 19:52:09 +0300

    fix: standardize quotation marks in migration file

--- app/migrations/1755013922893-UpdateExerciseTemplate.ts
+++ app/migrations/1755013922893-UpdateExerciseTemplate.ts
@@ -1,14 +1,14 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class UpdateExerciseTemplate1755013922893 implements MigrationInterface {
-    name = 'UpdateExerciseTemplate1755013922893'
+    name = 'UpdateExerciseTemplate1755013922893';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "exercise_template" DROP COLUMN "examples"`);
+        await queryRunner.query('ALTER TABLE "exercise_template" DROP COLUMN "examples"');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "exercise_template" ADD "examples" json array`);
+        await queryRunner.query('ALTER TABLE "exercise_template" ADD "examples" json array');
     }
 
 }
